c5df19044b7ea410c66875afcc5693c1e5d0ddd6
Deebakkarthi C R <dbk@deebakkarthi.com>
2026-04-08 19:14:18 -0400

    Add rudimentary zigfmt.sh

--- zigfmt.sh
+++ zigfmt.sh
@@ -1,3 +1,19 @@
 #!/usr/bin/env bash
 
-echo "$@"
+# No files
+# pre-commit will skip it if no files are matched
+# Just having this anyways as it doesn't hurt
+if [[ "$#" -eq 0 ]]; then
+	return 0
+fi
+
+# Exit on first error. You are not gonna commit anyways
+# You will run zig fmt after fixing the error anyways
+for file in "$@"; do
+	echo -ne "Formatting $file\n"
+	if ! zig fmt "$file"; then
+		exit 1
+	fi
+done
+
+exit 0
